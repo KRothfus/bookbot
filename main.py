a3add478154b5983712928635edeb43bf85b2c22
KRothfus <krothfus92@gmail.com>
2025-05-10 13:39:05 -0700

    Added find word count

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ def main():
 
 	def get_book_text(url):
 		with open(url) as f:
-			print( f.read())
-  
-	get_book_text("./frankenstein.txt")
-if "__name__" == "__main__":
-	main()
+			return f.read()
+	
+	def word_count(str):
+		count = 0
+		words = str.split()
+		for word in words:
+			count += 1
+		print(count)
+
+		return f'{count} words found in the document'
+
+	word_count(get_book_text("./books/frankenstein.txt"))
+main()
 
